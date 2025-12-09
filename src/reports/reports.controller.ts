f2d6c5dc605f1d6ff5c079bd1bc2d54152a4a2dd
MaimoonHossain <ashikasitosh740@gmail.com>
2025-12-09 18:11:41 +0600

    16-8 done

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -15,6 +15,7 @@ import { User } from 'src/users/user.entity';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { ReportDto } from './dtos/report.dto';
 import { ApproveReportDto } from './dtos/approve-report.dto';
+import { AdminGuard } from 'src/guards/admin.guard';
 
 @Controller('reports')
 export class ReportsController {
@@ -34,8 +35,8 @@ export class ReportsController {
   }
 
   @Patch('/:id')
-  @UseGuards(AuthGuard)
-  @Serialize(ReportDto)
+  @UseGuards(AdminGuard)
+  // @Serialize(ReportDto)
   approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
     return this.reportsService.changeApproval(id, body.approved);
   }
